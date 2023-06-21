04864d6ad8cb874fccd0a89f0052b3377343c404
Will de Deus <willzxxz@gmail.com>
2023-06-21 17:59:45 -0400

    adding allowedorigins

--- back-end/config/allowedOrigins.js
+++ back-end/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://auth-server-7jdu.onrender.com",
+];
 
 module.exports = allowedOrigins;
